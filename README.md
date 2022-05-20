75ac256b9f6f5ca853b13d4bdb2a3bbdffd76051
AnapalOne <91709385+AnapalOne@users.noreply.github.com>
2022-05-20 13:36:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,99 @@
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## A custom [xmonad](https://xmonad.org) configuration with clickable workspaces and easy naviagation, packed with tons of [pre-installed packages](https://github.com/AnapalOne/xmonad-packed#list-of-packages) for Arch Linux.
+
+![xmonad](https://github.com/AnapalOne/xmonad-packed/blob/main/50s5zbz9gqu81.png "xmonad")
+> **Note:** `xmonad.hs` will have configurations for packages not included in this setup. Generally these will not cause problems or decrease the performance of xmonad, but if the extra lines of code bother you, feel free to remove them if you like.
+
+## Requirements
+### git
+``` 
+pacman -S git
+```
+  - Verify: `git --version`
+
+### yay
+```
+git clone https://aur.archlinux.org/yay.git && cd yay
+```
+```
+pacman -S base-devel
+```
+```
+makepkg -si
+``` 
+
+## Installation
+#### Download
+``` 
+git clone https://github.com/AnapalOne/xmonad-packed && cd xmonad-packed
+``` 
+#### a) Applying a specific config file to your existing xmonad/program
+- If you want to only apply the xmonad config to your xmonad, simply copy `xmonad.hs` to `~/.xmonad/`
+- If you want to only apply xmobar, copy `xmobar` to `~/.xmobarrc`
+   - In this case, you will need to add some configs to `xmonad.hs logHook` in order for this xmobar to work. [Details found here](https://hackage.haskell.org/package/xmonad-contrib-0.17.0/docs/XMonad-Hooks-DynamicLog.html).
+- If you want to apply my configs to your programs, copy your selected config file (see `files` and `configs`) and apply it to your respective directory.
+
+#### or b) Installing this config and packages from base Arch Linux/existing window manager
+```
+chmod +x ./setup.sh
+```
+```
+./setup.sh
+```
+```
+reboot
+```
+
+### Display manager / xinit
+- use [xinit](https://wiki.archlinux.org/title/Xinit) to automatically start xmonad or a [display manager](https://wiki.archlinux.org/title/Display_manager) to cycle between window managers.
+   - Read the wiki for more details: https://wiki.archlinux.org/title/xmonad#Starting
+
+ ##### And you're done! You may edit any of these configs to your liking.
+
+## List of packages
+| Packages                                           | Description |
+| ---------------------------------------------------|:-------------:|
+| alacritty                                          | terminal emulator |
+| p7zip                                              | archiving and compression |
+| xmobar                                             | status bar |
+| picom-jonaburg-git                                 | compositor |
+| libreoffice                                        | office suite |
+| xf86-input-libinput, libinput-gestures             | input device handler, touchscreen, touchpad drivers |
+| pulseaudio                                         | audio manager |
+| pamixer                                            | volume control w/ gui |
+| youtube-dl                                         | youtube video/audio downloader |
+| ttf-iosevka[regular,aile,curly], ttf-bitstream-vera, ttf-nerd-fonts-symbols-mono, ttf-font | fonts |
+| xdotool                                            | input emulator |
+| htop                                               | program manager |
+| ncdu                                               | disk usage analyzer |
+| ranger, nemo                                       | file manager |
+| iwctl                                              | internet manager |
+| yay                                                | AUR helper |
+| subl, nano, vim                                    | text editors |
+| xprop                                              | shows information about a window |
+| flameshot                                          | capture screen |
+| tlp, tlpui                                         | battery manager |
+| nitrogen                                           | wallpaper manager |
+| firefox                                            | browser |
+| krita                                              | drawing software |
+| gimp                                               | image editor/drawing software |
+| mpv, vlc                                           | video software |
+| audacity                                           | audio software |
+| mirage                                             | image viewer |
+| spotify, cmus                                      | music |
+| discord                                            | text and voice app |
+| github-desktop                                     | github desktop |
+
+> See `setup.sh` for the full list of packages that will be installed.
+
+## Troubleshooting
+
+### Some programs not found
+- Packages may not install properly, so be sure to check and reinstall it after booting into xmonad.
+
+### picom not working
+- Try disabling vsync by changing the setting in `~/.config/picom/picom.conf`.
+- An error may occur as a result of your graphical drivers not working/compatible with picom, so try installing [Intel](https://wiki.archlinux.org/title/intel_graphics), [NVIDIA](https://wiki.archlinux.org/title/NVIDIA), or [other](https://wiki.archlinux.org/title/Hybrid_graphics) drivers depending on your GPU. 
+- picom will **not** work with VirtualBox, causing a sort of [HOM effect](https://doomwiki.org/wiki/Hall_of_mirrors_effect). To fix this, open the terminal with `Super + Shift + Enter` and type `killall picom` (note that you won't see any activity, so you might have do it blindly). As a result, there will be no compositor running but xmonad can be used without it.
+   - Currently working on a fix, stay tuned for updates.
---
+++ xmonad.hs
@@ -0,0 +1,310 @@
+---------------------------------------------------------
+-- XMonad Config by Anapal
+---------------------------------------------------------
+
+import XMonad
+import XMonad.Prompt -- add prompt in firefox bookmarks
+
+import Data.Monoid
+import System.Exit
+import System.IO
+import Graphics.X11.ExtraTypes.XF86
+
+import XMonad.Config.Desktop
+
+import XMonad.Actions.GridSelect
+import XMonad.Actions.CycleWS
+import XMonad.Actions.FloatKeys
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Grid
+import XMonad.Layout.Spiral
+--import XMonad.Layout.Magnifier
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Spacing
+
+import XMonad.ManageHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicProperty
+import XMonad.Hooks.ManageHelpers (doCenterFloat)
+
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Cursor
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+
+
+
+---------------------------------------------------------
+-- Configs
+---------------------------------------------------------
+
+myTerminal              = "alacritty"
+myModMask               = mod4Mask -- win key
+myBorderWidth           = 3
+myCursor                = xC_left_ptr
+
+myNormalBorderColor  = "#849DAB"
+myFocusedBorderColor = "#24788F"
+
+    -- grid applications (menu key)
+myGridSpawn = [ "subl","firefox","github-desktop",
+                "libreoffice","nemo","kdenlive",
+                "discord","spotify","gimp","krita","obs",
+                "kmix","deepin-calculator"]
+
+
+
+---------------------------------------------------------
+-- Workspaces
+---------------------------------------------------------
+
+myWorkspaces :: [String]
+myWorkspaces = clickable . (map xmobarEscape) 
+            -- $ ["ter","doc","www","dev","vid","img","chat","mus","art"] -- text
+            $ ["\xf120", "\xf718", "\xe743", "\xf121", "\xf008", "\xf03e", "\xf1d7", "\xf886", "\xf1fc"] -- icons
+    where
+          clickable l = [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
+                        (i,ws) <- zip [1..9] l,
+                        let n = i ]
+
+
+
+---------------------------------------------------------
+-- Key Binds
+-- > modm = Windows Key
+--
+-- Do xev | sed -ne '/^KeyPress/,/^$/p' for key maps.
+---------------------------------------------------------
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+ 
+    -- // windows
+    [ ((modm,            xK_BackSpace), kill)                               -- close focused window
+    , ((modm,               xK_space ), sendMessage NextLayout)             -- rotate layout
+    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf) -- reset layout order
+    , ((mod1Mask,           xK_Tab   ), windows W.focusUp     )             -- rotate focus between windows
+    , ((modm,               xK_Return), windows W.swapMaster  )             -- swap focus master and window
+    , ((modm .|. shiftMask, xK_comma ), sendMessage Shrink    )             -- decreases master window size
+    , ((modm .|. shiftMask, xK_period), sendMessage Expand    )             -- increases master window size
+    , ((modm,               xK_comma ), windows W.swapUp      )             -- move tiled window
+    , ((modm,               xK_period), windows W.swapDown    )             --
+
+    -- // floating windows
+    , ((modm .|. shiftMask, xK_Tab   ), withFocused toggleFloat)                        -- toggle between tiled and floating window
+    , ((modm,               xK_Up    ), withFocused (keysMoveWindow (0,-10)))           -- move floating window
+    , ((modm,               xK_Down  ), withFocused (keysMoveWindow (0,10)))            --
+    , ((modm,               xK_Left  ), withFocused (keysMoveWindow (-10,0)))           --
+    , ((modm,               xK_Right ), withFocused (keysMoveWindow (10,0)))            --
+    , ((modm .|. shiftMask, xK_Up    ), withFocused (keysResizeWindow (0,-10) (0,0)))   -- resize floating window
+    , ((modm .|. shiftMask, xK_Down  ), withFocused (keysResizeWindow (0,10) (0,0)))    --
+    , ((modm .|. shiftMask, xK_Left  ), withFocused (keysResizeWindow (-10,0) (0,0)))   --
+    , ((modm .|. shiftMask, xK_Right ), withFocused (keysResizeWindow (10,0) (0,0)))    --
+
+    -- // system commands
+    , ((modm,               xK_b     ), sendMessage ToggleStruts)                      -- toggle xmobar to front of screen
+    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))                     -- logout xmonad
+    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")  -- recompiles xmonad
+    , ((modm .|. shiftMask, xK_F1    ), spawn "systemctl hibernate")
+    , ((0,     xF86XK_MonBrightnessUp), spawn "lux -a 5%")
+    , ((0,   xF86XK_MonBrightnessDown), spawn "lux -s 5%")
+    , ((0,    xF86XK_AudioRaiseVolume), spawn "pamixer -i 5")
+    , ((0,    xF86XK_AudioLowerVolume), spawn "pamixer -d 5")
+    , ((0,           xF86XK_AudioMute), spawn "pamixer -t")
+
+    -- // programs
+    , ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)  -- open terminal
+    , ((modm .|. shiftMask, xK_s     ), spawn "flameshot gui")         -- equivelent to prntscr
+    , ((modm,               xK_r     ), spawn "dmenu_run")             -- run program
+    , ((modm .|. shiftMask, xK_r     ), spawn "gmrun")                 --
+    , ((modm .|. shiftMask, xK_v     ), spawn "alacritty -t alsamixer -e alsamixer")  -- sound system
+    
+    -- // scratchpad
+    , ((modm .|. controlMask, xK_Return), namedScratchpadAction myScratchpads "ScrP_alacritty")
+    , ((modm .|. shiftMask,   xK_slash ), namedScratchpadAction myScratchpads "help")
+    , ((modm,                 xK_grave ), namedScratchpadAction myScratchpads "ScrP_htop")
+    , ((modm .|. shiftMask,   xK_grave ), namedScratchpadAction myScratchpads "ScrP_ncdu")
+    , ((modm,                 xK_v     ), namedScratchpadAction myScratchpads "ScrP_vim")
+    , ((modm,                 xK_m     ), namedScratchpadAction myScratchpads "ScrP_cmus")
+    , ((modm .|. shiftMask,   xK_m     ), namedScratchpadAction myScratchpads "ScrP_spt")
+    , ((modm,                 xK_a     ), namedScratchpadAction myScratchpads "ScrP_trackma")
+
+    -- // grid
+    , ((modm,                 xK_Tab   ), goToSelected def)
+    , ((0,                    xK_Menu  ), spawnSelected def myGridSpawn)
+    ]
+    ++
+    -- mod-[1..9] = Switch to workspace 
+    -- mod-shift-[1..9] = Move window to workspace
+    [((m .|. modm, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+ 
+
+
+---------------------------------------------------------
+-- Layouts
+---------------------------------------------------------
+
+myLayout = avoidStruts
+        (smartBorders Full ||| spacingWithEdge 7 (Full ||| tiled ||| Mirror tiled ||| threecol ||| Mirror threecol ||| Grid ||| spiral (6/7)))
+  where
+     tiled = Tall nmaster delta ratio
+     nmaster = 1
+     delta = 3/100
+     ratio = 1/2
+     threecol = ThreeCol cnmaster cdelta cratio
+     cnmaster = 1
+     cdelta = 3/100
+     cratio = 1/2
+
+
+
+---------------------------------------------------------
+-- Scratchpads
+---------------------------------------------------------
+
+myScratchpads = 
+         [ NS "ScrP_alacritty" "alacritty -t scratchpad" (title =? "scratchpad") floatScratchpad
+         , NS "ScrP_htop" "alacritty -t htop -e htop" (title =? "htop") floatScratchpad
+         , NS "ScrP_vim" "alacritty -t vim -e vim" (title =? "vim") floatScratchpad
+         , NS "ScrP_ncdu" "alacritty -t ncdu -e ncdu" (title =? "ncdu") floatScratchpad
+         , NS "help" "~/.config/xmonad/scripts/help.sh" (title =? "list of programs") floatScratchpad
+         , NS "ScrP_cmus" "alacritty -t cmus -e cmus" (title =? "cmus") floatScratchpad
+         , NS "ScrP_spt" "alacritty -t spotify-tui -e spt" (title =? "spotify-tui") floatScratchpad
+         , NS "ScrP_trackma" "alacritty -t trackma-curses -e trackma-curses" (title =? "trackma-curses") floatScratchpad
+         ]
+    where 
+       floatScratchpad = customFloating $ W.RationalRect l t w h
+                where
+                    h = 0.9
+                    w = 0.9
+                    l = 0.95 -h
+                    t = 0.95 -w
+
+
+
+---------------------------------------------------------
+-- Hooks
+---------------------------------------------------------
+
+        --Parameters for certain programs
+        -- > doFloat to open in floating mode
+        -- > doShift to open only in a specific workspace
+myManageHook :: Query (Data.Monoid.Endo WindowSet)
+myManageHook = composeAll
+
+        -- Opens the program only on a specific workspace.
+        -- NOTE: It will not work when the workspaces in myWorkspace does not match the doShift command here. 
+        --       To fix this, replace the workspace inside doShift with your renamed workspace.
+
+        -- ter 
+        [ title     =? "alacritty"      --> doShift "<action=xdotool key super+1>\xf120</action>"
+        -- doc
+        , className =? "Subl"           --> doShift "<action=xdotool key super+2>\xf718</action>" 
+        , className =? "libreoffice-startcenter" --> doShift "<action=xdotool key super+2>\xf718</action>"
+        , className =? "calibre"        --> doShift "<action=xdotool key super+2>\xf718</action>" 
+        --www
+        , className =? "firefox"        --> doShift "<action=xdotool key super+3>\xe743</action>" 
+        , className =? "Chromium"       --> doShift "<action=xdotool key super+3>\xe743</action>"
+        --dev
+        , className =? "Audacity"       --> doShift "<action=xdotool key super+4>\xf121</action>" 
+        , className =? "GitHub Desktop" --> doShift "<action=xdotool key super+4>\xf121</action>" 
+        , className =? "Code"           --> doShift "<action=xdotool key super+4>\xf121</action>" 
+        --vid
+        , className =? "vlc"            --> doShift "<action=xdotool key super+5>\xf008</action>" 
+        , className =? "mpv"            --> doShift "<action=xdotool key super+5>\xf008</action>" 
+        , className =? "kdenlive"       --> doShift "<action=xdotool key super+5>\xf008</action>" 
+        --img
+        , className =? "Gimp"           --> doShift "<action=xdotool key super+6>\xf03e</action>" 
+        , className =? "Mirage"         --> doShift "<action=xdotool key super+6>\xf03e</action>" 
+        --chat
+        , className =? "discord"        --> doShift "<action=xdotool key super+7>\xf1d7</action>" 
+        --art
+        , className =? "krita"          --> doShift "<action=xdotool key super+9>\xf1fc</action>" 
+
+        -- Places the window in floating mode.
+        , className =? "Nemo"           --> doCenterFloat
+        , className =? "kmix"           --> doFloat
+        , title     =? "alsamixer"      --> doCenterFloat
+        , className =? "Sxiv"           --> doFloat
+        , title     =? "welcome"        --> doCenterFloat
+        ]
+
+        -- Spotify's window name apparently cannot be found, so this is a workaround.
+spotifyWindowNameFix = dynamicPropertyChange "WM_NAME" (title =? "Spotify" --> doShift "<action=xdotool key super+8>\xf886</action>") --mus
+
+        --event handling
+myEventHook = fullscreenEventHook <+> spotifyWindowNameFix
+
+        --Executes whenever xmonad starts
+myStartupHook = do
+        spawnOnce "nitrogen --restore &"
+        spawnOnce "picom &"
+        spawnOnce "~/.config/xmonad/scripts/startup_screen.sh"
+        spawnOnce "libinput-gestures &"
+        setDefaultCursor myCursor
+
+
+
+---------------------------------------------------------
+-- XMonad Main
+---------------------------------------------------------
+
+main = do
+   xmproc <- spawnPipe "xmobar -x 0 ~/.xmobarrc/xmobar"
+   xmonad $ docks $ ewmh desktopConfig
+        { terminal           = myTerminal
+        , modMask            = myModMask
+        , workspaces         = myWorkspaces
+        , borderWidth        = myBorderWidth
+        , normalBorderColor  = myNormalBorderColor
+        , focusedBorderColor = myFocusedBorderColor
+
+        , keys               = myKeys
+
+        , layoutHook         = myLayout
+        , manageHook         = myManageHook <+> namedScratchpadManageHook myScratchpads
+        , handleEventHook    = myEventHook
+        , logHook            = dynamicLogWithPP . namedScratchpadFilterOutWorkspacePP $ def
+                                   { ppOutput = hPutStrLn xmproc
+                                   , ppCurrent = xmobarColor "#4381fb" "" . wrap "[" "]"
+                                   , ppVisible = xmobarColor "#4381fb" ""
+                                   , ppHidden = xmobarColor "#d1426e" "" . wrap "*" ""
+                                   , ppHiddenNoWindows = xmobarColor "#061d8e" ""
+                                   , ppTitle = xmobarColor "#ffffff" "" . shorten 60
+                                   , ppSep = "<fc=#666666> | </fc>"
+                                   , ppWsSep = "<fc=#666666> . </fc>"
+                                   , ppExtras = [windowCount]
+                                   , ppOrder = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+                                }
+
+        , startupHook        = myStartupHook
+     }
+
+
+
+---------------------------------------------------------
+-- Functions
+---------------------------------------------------------
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
+
+toggleFloat :: Window -> X ()
+toggleFloat w = windows
+   ( \s -> if M.member w (W.floating s)
+           then W.sink w s
+           else (W.float w (W.RationalRect (0.01) (0.04) (0.55) (0.55)) s) )
+
+xmobarEscape :: String -> String
+xmobarEscape = concatMap doubleLts
+   where
+           doubleLts '<' = "<<"
+           doubleLts x   = [x]
