1bada3224609915f4696c409acfc25d52744febc
AnapalOne <91709385+AnapalOne@users.noreply.github.com>
2022-05-27 17:10:13 +0800

    Added desktop notifs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Install packages
-bash -c 'sudo pacman -S --noconfirm xmonad xmonad-contrib alacritty xterm wget dmenu xf86-input-libinput xmobar neofetch htop ncdu vim nano xorg xorg-xinit p7zip flameshot pulseaudio-alsa pulseaudio-bluetooth pamixer tlp youtube-dl ranger cmus nitrogen firefox krita xdotool gimp mpv vlc audacity discord libreoffice-still ttc-iosevka ttc-iosevka-aile ttc-iosevka-curly ttf-bitstream-vera ttf-font ttf-nerd-fonts-symbols-mono retroarch pcsx2 nethack pychess kdenlive deepin-calculator code blueman obs-studio easytag calibre qbittorrent spotifyd nemo bc alsa-utils'
-bash -c 'yay -S --mflags --skipinteg --answerclean All --answerdiff All --answeredit All --answerupgrade Repo ani-cli chocolate-doom tlpui-git spotify picom-jonaburg-git github-desktop-bin mirage libinput-gestures sublime-text-4 bashmount spotify-tui trackma'
+bash -c 'sudo pacman -S --noconfirm xmonad xmonad-contrib alacritty xterm wget dmenu xf86-input-libinput xmobar neofetch htop ncdu vim nano xorg xorg-xinit p7zip flameshot pulseaudio-alsa pulseaudio-bluetooth pamixer tlp youtube-dl ranger cmus nitrogen firefox krita xdotool gimp mpv vlc audacity discord libreoffice-still ttc-iosevka ttc-iosevka-aile ttc-iosevka-curly ttf-bitstream-vera ttf-font ttf-nerd-fonts-symbols-mono retroarch pcsx2 nethack pychess kdenlive deepin-calculator code blueman obs-studio easytag calibre qbittorrent spotifyd nemo bc alsa-utils dunst'
+bash -c 'yay -S --mflags --skipinteg --answerclean All --answerdiff All --answeredit All --answerupgrade Repo ani-cli chocolate-doom tlpui-git spotify picom-jonaburg-git github-desktop-bin mirage libinput-gestures sublime-text-4 bashmount spotify-tui trackma cmus-notify'
 
 # Move configs and files to directories
 bash -c 'mkdir ~/.xmonad && cp xmonad.hs ~/.xmonad/'
@@ -11,6 +11,7 @@ bash -c 'mkdir ~/.config/picom && cp config/picom.conf ~/.config/picom/'
 bash -c 'mkdir ~/.config/neofetch && cp config/neofetch_config.conf ~/.config/neofetch/config.conf'
 bash -c 'mkdir -p ~/Pictures/Ascii/ && cp files/archlinux.txt ~/Pictures/Ascii/'
 bash -c 'cp bashrc ~/.bashrc'
+bash -c 'mkdir ~/.config/dunst && cp /etc/dunst/dunstrc ~/.config/dunst/dunstrc'
 
 # Add execution permissions to shell scripts
 bash -c 'cp config/shell/help.sh config/shell/startup_screen.sh ~/.config/xmonad/scripts/ && chmod +x ~/.config/xmonad/scripts/help.sh && chmod +x ~/.config/xmonad/scripts/startup_screen.sh'
