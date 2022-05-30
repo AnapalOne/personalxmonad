c917a49defecea41b092be0fc9bd59cb4653db66
AnapalOne <91709385+AnapalOne@users.noreply.github.com>
2022-05-31 02:34:27 +0800

    Updated battery script

--- config/shell/battery_notifs.sh
+++ config/shell/battery_notifs.sh
@@ -5,12 +5,15 @@
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 
 while true; do
-	if [ $battery -eq 10 ]; then
-			notify-send --urgency=critical "Critical Battery! (10%)" "Charge your laptop quickly. Before you lose your data. Seriously."
+	if [ $battery -eq 15 ]; then
+			notify-send --urgency=critical "Low Battery! (15%)" "Charge your laptop quickly."
 			espeak "Low battery. Please charge."
+	elif [ $battery -eq 10 ]; then
+			notify-send --urgency=critical "Critical Battery! (10%)" "Charge your laptop now. Before you lose your data. Seriously."
+			espeak "Critical battery. Please charge."
 	elif [ $battery -eq 5 ]; then
 			notify-send --urgency=critical "REALLY Critical Battery! (5%)" "Dude, your battery is going to go to the landfill soon."
-			espeak "Critical battery. Please charge."
+			espeak "Critical battery. Charge immediately."
 	fi
 	echo "$battery"
 	sleep 5
